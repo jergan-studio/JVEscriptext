48ec2b1e13fd0beb86ff3d80faa8563c6f06aae6
jergan-studio <jose343lopez22@gmail.com>
2026-03-12 21:27:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,67 +8,82 @@ const app = express()
 app.use(express.json())
 app.use(express.static("public"))
 
-app.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname,"public","index.html"))
-})
-
-// Function to run all .bJVE scripts in a repo folder
-function runAllScripts(deployPath){
-    return new Promise((resolve, reject)=>{
-        glob(`${deployPath}/**/*.bJVE`, (err, files)=>{
-            if(err) return reject(err)
-            if(files.length===0) return reject("No .bJVE scripts found!")
-
-            let outputs = []
-            let promises = files.map(file=>{
-                return new Promise(res=>{
-                    exec(`node interpreter.js "${file}"`, (err, stdout)=>{
-                        outputs.push(stdout)
-                        res()
-                    })
-                })
-            })
-
-            Promise.all(promises).then(()=>resolve(outputs.join("\n")))
-        })
+// log everything
+function safeExec(cmd){
+  console.log("Running:", cmd)
+  return new Promise((resolve,reject)=>{
+    exec(cmd, (err,stdout,stderr)=>{
+      if(err){
+        console.error("ERROR:",stderr)
+        reject(stderr)
+      } else {
+        console.log("OK:",stdout)
+        resolve(stdout)
+      }
     })
+  })
 }
 
-// Deploy endpoint
-app.post("/deploy", async (req,res)=>{
-    const repo = req.body.repo
-    if(!repo) return res.send("No repo provided")
-    const repoName = repo.split("/").pop().replace(".git","")
-    const deployPath = path.join(__dirname,"deployed_repos",repoName)
+app.get("/", (req,res)=>{
+  res.sendFile(path.join(__dirname,"public","index.html"))
+})
 
-    exec(`rm -rf "${deployPath}" && git clone ${repo} "${deployPath}"`, async (err)=>{
-        if(err) return res.send("Error cloning repo")
+function runAllScripts(deployPath){
+  return new Promise((resolve, reject)=>{
+    glob(`${deployPath}/**/*.bJVE`, (err, files)=>{
+      if(err) return reject(err)
+      if(files.length===0) return reject("No .bJVE scripts found!")
 
-        try{
-            const output = await runAllScripts(deployPath)
-            const clientPath = path.join(deployPath,"index.html")
-            let result = output + "\n"
-            if(fs.existsSync(clientPath)){
-                result += `Play client here: /play/${repoName}`
-            }
-            res.send(result)
-        }catch(e){
-            res.send(e.toString())
-        }
+      let outputs=[]
+      let tasks=files.map(file=>{
+        return safeExec(`node interpreter.js "${file}"`).then(o=>{
+          outputs.push(o)
+        }).catch(e=>{
+          outputs.push("Error in script: "+e)
+        })
+      })
+
+      Promise.all(tasks).then(()=>{
+        resolve(outputs.join("\n"))
+      })
     })
-})
+  })
+}
 
-// Serve repo client pages
-app.get("/play/:repo", (req,res)=>{
-    const repoName = req.params.repo
-    const clientPath = path.join(__dirname,"deployed_repos",repoName,"index.html")
+app.post("/deploy", async (req,res)=>{
+  const repo=req.body.repo
+  if(!repo) return res.send("No repo provided")
+  const repoName = repo.split("/").pop().replace(".git","")
+  const deployPath = path.join(__dirname,"deployed_repos",repoName)
+
+  // safer cleanup
+  if(fs.existsSync(deployPath)){
+    fs.rmSync(deployPath,{recursive:true,force:true})
+  }
+
+  try{
+    await safeExec(`git clone ${repo} "${deployPath}"`)
+    const output = await runAllScripts(deployPath)
+    let responseText = output + "\n"
+    const clientPath = path.join(deployPath,"index.html")
     if(fs.existsSync(clientPath)){
-        res.sendFile(clientPath)
-    }else{
-        res.send("Client page not found")
+      responseText += `Play client here: /play/${repoName}`
     }
+    res.send(responseText)
+  }catch(e){
+    res.send("Deploy error: "+e)
+  }
 })
 
-app.listen(3000, ()=>{
-    console.log("JVEscriptext running on port 3000")
+app.get("/play/:repo", (req,res)=>{
+  const clientPath = path.join(__dirname,"deployed_repos",req.params.repo,"index.html")
+  if(fs.existsSync(clientPath)){
+    res.sendFile(clientPath)
+  } else {
+    res.send("Client page not found")
+  }
+})
+
+app.listen(3000,()=>{
+  console.log("JVEscriptext running")
 })
