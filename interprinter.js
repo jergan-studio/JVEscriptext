77ab602d1c63c89fac90044b99c13f1321cc8554
jergan-studio <jose343lopez22@gmail.com>
2026-03-12 21:18:50 -0500

    Update interprinter.js

--- interprinter.js
+++ interprinter.js
@@ -1,37 +1,39 @@
 const fs = require("fs")
+const fetch = (...args) => import('node-fetch').then(({default: f}) => f(...args))
 
-function runJVE(file){
+async function runJVE(file){
+    const code = fs.readFileSync(file,"utf8")
+    const lines = code.split("\n")
 
-const code = fs.readFileSync(file,"utf8")
-const lines = code.split("\n")
+    for(const lineRaw of lines){
+        const line = lineRaw.trim()
+        if(line.startsWith("print")){
+            const msg = line.replace("print ","").replace(/"/g,"")
+            console.log(msg)
+        }
 
-lines.forEach(line=>{
+        if(line.startsWith("lua.wait")){
+            const t = parseInt(line.split(" ")[1] || 1)
+            await new Promise(r=>setTimeout(r, t*1000))
+        }
 
-line=line.trim()
+        if(line.startsWith("server.get")){
+            const url = line.split(" ")[1].replace(/"/g,"")
+            try{
+                const res = await fetch(url)
+                const text = await res.text()
+                console.log(text)
+            }catch(e){
+                console.log("Error fetching", url)
+            }
+        }
 
-if(line.startsWith("print")){
-let msg=line.replace("print ","")
-console.log(msg.replace(/"/g,""))
+        if(line.startsWith("js.run")){
+            const js = line.split(" ").slice(1).join(" ").replace(/"/g,"")
+            console.log("JS to run:", js)
+        }
+    }
 }
 
-if(line.startsWith("server.get")){
-let url=line.split(" ")[1].replace(/"/g,"")
-
-fetch(url)
-.then(res=>res.text())
-.then(data=>console.log(data))
-}
-
-if(line.startsWith("lua.wait")){
-let t=parseInt(line.split(" ")[1])
-
-setTimeout(()=>{
-console.log("Waited "+t+" seconds")
-},t*1000)
-}
-
-})
-
-}
-
+// Run the file from command line
 runJVE(process.argv[2])
