5c52c91619e96ce659e9c5c1d0bf7431624781dd
jergan-studio <jose343lopez22@gmail.com>
2026-03-13 12:17:42 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-app.use(express.static("public"))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname,"public","index.html"))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
-    console.log("JVEscriptext running on port " + PORT)
-})
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("JVEscriptext running on port " + PORT);
+});
